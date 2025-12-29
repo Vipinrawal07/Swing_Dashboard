05637a9d6cb2fc28dfa425887e0bc536e9533aed
Vipinrawal07 <vipin_rawal07@yahoo.co.in>
2025-12-29 20:17:26 +0530

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -1,1 +1,18 @@
+import yfinance as yf
+import pandas as pd
+import streamlit as st
 
+@st.cache_data(ttl=86400)
+def get_nse_stock_list():
+    url = "https://archives.nseindia.com/content/equities/EQUITY_L.csv"
+    df = pd.read_csv(url)
+    return (df["SYMBOL"] + ".NS").tolist()
+
+
+@st.cache_data(ttl=3600)
+def load_price_data(ticker):
+    try:
+        df = yf.download(ticker, period="2y", interval="1d", progress=False)
+        return df if not df.empty else None
+    except:
+        return None
