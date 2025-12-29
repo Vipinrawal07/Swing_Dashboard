a920782f4377de235354abae0a259041b1096ee7
Vipinrawal07 <vipin_rawal07@yahoo.co.in>
2025-12-29 20:13:01 +0530

    Delete sentiment directory

--- sentiment/news_sentiment.py
+++
@@ -1,9 +0,0 @@
-import feedparser
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-analyzer = SentimentIntensityAnalyzer()
-
-def news_sentiment(q):
-    feed = feedparser.parse(f"https://news.google.com/rss/search?q={q}+India+stock")
-    scores = [analyzer.polarity_scores(e.title)["compound"] for e in feed.entries[:30]]
-    return sum(scores)/len(scores) if scores else 0
